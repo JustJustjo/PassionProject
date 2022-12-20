c634996ee1f87854a53cf3ee7f9961b65fbf0133
justin <justin@kilk.com>
2022-12-19 16:38:03 -0800

    Added drawing panel

--- src/main/kotlin/org/team2471/frc/pathvisualizer/FieldPane.kt
+++ src/main/kotlin/org/team2471/frc/pathvisualizer/FieldPane.kt
@@ -1,18 +1,20 @@
 package org.team2471.frc.pathvisualizer
 
-import javafx.scene.input.*
-import javafx.scene.layout.StackPane
-import javafx.scene.shape.Circle
-
-object FieldPane : StackPane() {
-
-    val circle = Circle(300.0, 135.0, 100.0)
-
-    circle
+import javafx.scene.Group
+import javafx.scene.canvas.Canvas
+import javafx.scene.paint.Color
 
 
-    private fun onMouseClick(e: MouseEvent) {
-        println("Mouse moved event")
+object FieldPane : Group() {
+    init {
+
+        val canvas = Canvas(740.0,740.0)
+        val gc = canvas.graphicsContext2D
+        gc.setFill(Color.LIGHTGRAY)
+//        gc.fillOval(0.0, 0.0, 740.0, 740.0)
+        gc.fillRect(0.0,0.0,740.0,740.0)
+
+        FieldPane.getChildren().add(canvas)
     }
 
 }
--- src/main/kotlin/org/team2471/frc/pathvisualizer/PathVisualizer.kt
+++ src/main/kotlin/org/team2471/frc/pathvisualizer/PathVisualizer.kt
@@ -2,10 +2,13 @@ package org.team2471.frc.pathvisualizer
 
 import javafx.application.Application
 import javafx.geometry.Rectangle2D
+import javafx.scene.Group
 import javafx.scene.Scene
+import javafx.scene.canvas.Canvas
 import javafx.scene.control.Button
 import javafx.scene.control.ButtonBar
 import javafx.scene.layout.BorderPane
+import javafx.scene.paint.Color
 import javafx.stage.Screen
 import javafx.stage.Stage
 
@@ -25,26 +28,17 @@ class PathVisualizer : Application() {
         stage.title = "Path Visualizer"
         PathVisualizer.stage = stage
 
-        val hellobutton = Button()
-        hellobutton.setOnAction {
-            println("HELLO")
-        }
-        hellobutton.height + 10
-        hellobutton.width + 10
-        hellobutton.contentDisplay
-
-
-        val borderPane = BorderPane()
-        borderPane.top = TopBar
-        borderPane.top = ControlPanel
 
         val screen = Screen.getPrimary()
 
+        val borderPane = BorderPane(FieldPane)
+        borderPane.top = TopBar
+        borderPane.top = ControlPanel
+
+
         val bounds = Rectangle2D(screen.visualBounds.minX, screen.visualBounds.minY, screen.visualBounds.width, screen.visualBounds.height - 30)
         stage.scene = Scene(borderPane, bounds.width, bounds.height)
-        stage.scene.stylesheets.add("assets/theme.css")
         stage.sizeToScene()
-//        stage.isMaximized = true
         stage.show()
     }
 }
