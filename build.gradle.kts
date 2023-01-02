949b0ac8ea59b5c48ac686bfd65e19242db2817e
Justin Kilk <justin@kilk.com>
2023-01-02 12:34:48 -0800

    Java 8 branch, runs with java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.gradle.jvm.tasks.Jar
 
 plugins {
-    java
+    kotlin("jvm") version "1.7.21"
     application
-    kotlin("jvm") version "1.5.31"
-    id("com.github.gmazzo.buildconfig") version "3.0.0"
-    id("org.openjfx.javafxplugin") version "0.0.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
 }
 val mainClass = "org.team2471.frc.pathvisualizer.Main"
 
@@ -26,47 +29,18 @@ tasks {
     }
 }
 
-val wpiLibVersion = "2022.1.1"
-repositories {
-    mavenCentral()
-    maven { setUrl("https://frcmaven.wpi.edu/artifactory/release/")}
-    maven { setUrl("https://plugins.gradle.org/m2/")}
-    maven { setUrl("https://maven.ctr-electronics.com/release/") }
-    maven { setUrl("https://maven.revrobotics.com/") }
-}
-
-javafx {
-    version = "17"
-    modules = "javafx.controls".split(",").toMutableList()
-}
-application {
-    mainClass.set("org.team2471.frc.pathvisualizer.Main")
-}
-
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
-    implementation("edu.wpi.first.ntcore:ntcore-java:$wpiLibVersion")
-    implementation("edu.wpi.first.ntcore:ntcore-jni:$wpiLibVersion")
-    implementation("edu.wpi.first.ntcore:ntcore-jni:$wpiLibVersion:windowsx86-64")
-    implementation("edu.wpi.first.ntcore:ntcore-jni:$wpiLibVersion:osxx86-64")
-    implementation("edu.wpi.first.wpiutil:wpiutil-java:$wpiLibVersion")
-    implementation("edu.wpi.first.wpimath:wpimath-java:$wpiLibVersion")
-    implementation("edu.wpi.first.wpilibj:wpilibj-java:$wpiLibVersion")
-//    implementation("org.team2471.lib:meanlib")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("com.github.gmazzo.buildconfig:com.github.gmazzo.buildconfig.gradle.plugin:3.0.0")
-    implementation("com.google.code.gson:gson:2.8.9")
-}
-java {
-    withSourcesJar()
-    withJavadocJar()
+    testImplementation(kotlin("test"))
 }
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+tasks.test {
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("org.team2471.frc.pathvisualizer.Main")
 }
